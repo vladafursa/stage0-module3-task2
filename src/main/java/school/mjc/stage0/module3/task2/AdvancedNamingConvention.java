83bc59853da582cbf619da758adc2d9fd248f02b
T0337485 <t0337485@my.ntu.ac.uk>
2023-10-22 22:53:16 +0100

    task 2 completed

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.module3.task2;
 
-public class advancedNamingConvention {
-    private static final int adult_age = 18;
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
     private int AGE;
     private int phoneNumber;
--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
+        int v_ariable = 10;
         int variable_One = 1;
         int variableTwo = 2;
         int variable_three = 3;
