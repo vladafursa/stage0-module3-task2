88ffbf64150f0df5878cc1c7a316cb78d7454baa
T0337485 <t0337485@my.ntu.ac.uk>
2023-10-22 22:57:53 +0100

    fixed issues

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -3,14 +3,14 @@ package school.mjc.stage0.module3.task2;
 public class AdvancedNamingConvention {
     private static final int ADULT_AGE = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void callByNumber(int number) {
         //some code
     }
 }
